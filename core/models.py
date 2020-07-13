6a3865659200c0645ffbca543b99a2c12466f1dc
Marcelo Horita <mfhorita@gmail.com>
2020-07-12 22:57:11 -0300

    Add Agenda para deploy Heroku

---
+++ core/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from datetime import datetime as dt
+from django.db import models
+
+
+# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
+    class Meta:
+        db_table = 'evento'
+
+    
+    def __str__(self):
+        return self.titulo
+
+
+    def get_data_evento(self):
+        return self.data_evento.strftime("%d/%m/%Y %H:%M")
+
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')    
+
+    def get_evento_atual(self):
+        if self.data_evento.strftime('%d%m%Y') == dt.today().strftime('%d%m%Y'):
+            return True
+        else:
+            return False
