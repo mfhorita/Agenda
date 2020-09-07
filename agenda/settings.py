6376a6fed09731e6b60f35f1bcc981ce20ce064d
Marcelo Horita <mfhorita@gmail.com>
2020-09-06 23:28:01 -0300

    Update settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'agenda.urls'
