92c3f9215f953b1feae72e119cab267197db047e
Marcelo Horita <mfhorita@gmail.com>
2020-12-13 22:32:43 -0300

    Automatic commit

--- core/models.py
+++ core/models.py
@@ -12,19 +12,15 @@ class Evento(models.Model):
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    
     class Meta:
         db_table = 'evento'
 
-    
     def __str__(self):
         return self.titulo
-
 
     def get_data_evento(self):
         return self.data_evento.strftime("%d/%m/%Y %H:%M")
 
-    
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')    
 
--- core/views.py
+++ core/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http.response import Http404, JsonResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from datetime import datetime as dt
 from django.contrib import messages
 from core.models import Evento
